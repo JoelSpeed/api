2f06b86bd6f5ed706dc2cd02f6e1197865a3b368
Joel Speed <joel.speed@hotmail.co.uk>
2022-08-16 21:10:18 +0100

    Update verify compatibility script for OSX compatibility
    This script couldn't run on OSX because of differences between mktemp 
    versions and a lack of mapfile by default.
    
    This resolves those issues and optimises the verify loop which 
    previously iterated over all go files (including vendor) and not just 
    those in the folders the compatibility gen touches

--- hack/verify-compatibility.sh
+++ hack/verify-compatibility.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#shellcheck source=lib/init.sh
+
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-TMP_ROOT=$(mktemp --directory)
+# Linux vs OSX have different versions of mktemp which have different requirements.
+# This attempts the Linux version first and then, should that fail, attempts the OSX version.
+# Xref: https://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
+TMP_ROOT=$(mktemp --directory 2>/dev/null || mktemp -d -t 'api-compatibility')
 
 cleanup() {
   rm -rf "${TMP_ROOT}"
@@ -11,19 +14,23 @@ trap "cleanup" EXIT SIGINT
 
 V_ROOT="${TMP_ROOT}/src/github.com/openshift/api"
 mkdir -p "$V_ROOT"
-cp -a --no-preserve=timestamp -r "$SCRIPT_ROOT"/* "$V_ROOT"
+cp -a "$SCRIPT_ROOT"/* "$V_ROOT"
 (
   cd "$V_ROOT" || exit
   export GOPATH="$TMP_ROOT"
   rm -Rf _output
-#  bash --init-file <(echo cd $V_ROOT)
   ./hack/update-compatibility.sh > /dev/null
 )
 
-mapfile -t GENERATED < <(find "${SCRIPT_ROOT}" -name "*.go" -printf "%P\n" | sort)
-for g in "${GENERATED[@]}" ; do
-  if ! diff --unified --text "$SCRIPT_ROOT/$g" "$V_ROOT/$g" ; then
-    printf "\nopenshift_compatibility is out of date. Please run hack/update-compatibility.sh\n"
-    exit 1
-  fi
+# The compatibility script only generates based on the contents of API_GROUP_VERSIONS.
+# We can limit our comparisons to these folders.
+for apiVersion in ${API_GROUP_VERSIONS}; do
+  GENERATED=$(find "${SCRIPT_ROOT}/${apiVersion}" -name "*.go" | sed "s|^${SCRIPT_ROOT}/||" | sort)
+
+  for g in ${GENERATED}; do
+    if ! diff --unified --text "$SCRIPT_ROOT/$g" "$V_ROOT/$g" ; then
+      printf "\nopenshift_compatibility is out of date for ${apiVersion}. Please run hack/update-compatibility.sh\n"
+      exit 1
+    fi
+  done
 done
