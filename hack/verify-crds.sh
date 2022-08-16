1a97035e815c188704a1c4fd90e541d43c93c92d
Joel Speed <joel.speed@hotmail.co.uk>
2022-08-16 21:28:10 +0100

    Move verify-crds to use vendored YQ

--- hack/verify-crds.sh
+++ hack/verify-crds.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-if [ ! -f ./_output/tools/bin/yq ]; then
-    mkdir -p ./_output/tools/bin
-    curl -s -f -L https://github.com/mikefarah/yq/releases/download/2.4.0/yq_$(go env GOHOSTOS)_$(go env GOHOSTARCH) -o ./_output/tools/bin/yq
-    chmod +x ./_output/tools/bin/yq
-fi
+source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
 FAILS=false
 
-for f in $(find . -name "*.yaml" -type f); do
+for f in $(find "${SCRIPT_ROOT}" -name "*.yaml" -type f); do
     grep -qre "kind:\(.*\)CustomResourceDefinition" $f || continue
     grep -qre "name:\(.*\).openshift.io" $f || continue
 
-    if [[ $(./_output/tools/bin/yq r $f apiVersion) == "apiextensions.k8s.io/v1beta1" ]]; then
-        if [[ $(./_output/tools/bin/yq r $f spec.validation.openAPIV3Schema.properties.metadata.description) != "null" ]]; then
+    echo Verifiying $f
+
+    if [[ $(${YQ} '.apiVersion' $f) == "apiextensions.k8s.io/v1beta1" ]]; then
+        if [[ $(${YQ} '.spec.validation.openAPIV3Schema.properties.metadata.description' $f) != "null" ]]; then
             echo "Error: cannot have a metadata description in $f"
             FAILS=true
         fi
 
-        if [[ $(./_output/tools/bin/yq r $f spec.preserveUnknownFields) != "false" ]]; then
-            echo "Error: pruning not enabled (spec.preserveUnknownFields != false) in $f"
+        if [[ $(${YQ} '.spec.preserveUnknownFields' $f) != "false" ]]; then
+            echo "Error: pruning not enabled (.spec.preserveUnknownFields != false) in $f"
             FAILS=true
         fi
     fi
 
-    if [[ $(./_output/tools/bin/yq r $f metadata.annotations[api-approved.openshift.io]) == "null" ]]; then
+    if [[ $(${YQ} '.metadata.annotations["api-approved.openshift.io"]' $f) == "null" ]]; then
         echo "Error: missing 'api-approved.openshift.io' annotation pointing to openshift/api pull request in $f"
         FAILS=true
     fi
@@ -33,4 +31,3 @@ done
 if [ "$FAILS" = true ] ; then
     exit 1
 fi
-
